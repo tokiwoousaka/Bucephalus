ce3a90af78addb5be202853ea633912bd65cce02
Tokiwo-ousaka <its.out.of.tune.this.my.music@gmail.com>
2012-09-20 01:27:10 +0900

    ファイル作成

---
+++ Graphics/Bucephalus/Core.hs
@@ -0,0 +1,4 @@
+module Graphics.Bucephalus.Core(test) where
+
+test :: String
+test = "Hello, Bucephalus!"
---
+++ Test/Core.hs
@@ -0,0 +1,4 @@
+module Test where
+import Graphics.Bucephalus.Core
+
+main = putStrLn test
