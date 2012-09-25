ae8757f57ce2b928267674d6667f40e2844c232f
Tokiwo-ousaka <its.out.of.tune.this.my.music@gmail.com>
2012-09-26 00:21:18 +0900

    ゲームパッドの型をButtonsからButtonに変更

---
+++ Graphics/UI/Bucephalus/Type/Pads.hs
@@ -0,0 +1,37 @@
+
+module Graphics.UI.Bucephalus.Type.Pads(
+  ) where
+
+import qualified Graphics.UI.SDL as SDL
+
+---------------------------------------------------------------------------------------------------
+-- ゲームパッド型クラス Bucephalus内部で受け渡しされるゲームパッドやキーボードの状況
+---------------------------------------------------------------------------------------------------
+
+class GamePad p where
+  padInit :: p
+  interpretPadEvent :: p -> SDL.Event -> p
+
+---------------------------------------------------------------------------------------------------
+-- 基本的なゲームボタン型をデフォルトで提供
+---------------------------------------------------------------------------------------------------
+
+data StanderdPad = StanderdPad {
+  buttonUp :: Integer,
+  buttonDown :: Integer,
+  buttonLeft :: Integer,
+  buttonRight :: Integer,
+  buttonA :: Integer,
+  buttonB :: Integer,
+  buttonX :: Integer,
+  buttonY :: Integer,
+  buttonL :: Integer,
+  buttonR :: Integer,
+  buttonStart :: Integer,
+  buttonSelect :: Integer
+  } deriving (Show, Eq)
+
+instance GamePad StanderdPad where
+  padInit = StanderdPad 0 0 0 0 0 0 0 0 0 0 0 0
+
+  interpretPadEvent pad _ = pad 
