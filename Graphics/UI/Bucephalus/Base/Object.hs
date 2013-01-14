5f911cd1a5aa6142fe4a7b762936a6701bb0222a
Tokiwo-ousaka <its.out.of.tune.this.my.music@gmail.com>
2013-01-15 02:38:41 +0900

    複数の当たり判定枠を一括で扱うための構造を暫定的に作成

--- Graphics/UI/Bucephalus/Base/Object.hs
+++ Graphics/UI/Bucephalus/Base/Object.hs
@@ -1,36 +1,43 @@
 
 module Graphics.UI.Bucephalus.Base.Object(
-  ObjectState(..),
-  StandardObjectState(..)
+  ObjectState(..)
   ) where
 import Graphics.UI.Bucephalus.Type.Collision
 import Graphics.UI.Bucephalus.Type.Surface
 
---Functor ((,) a) を下位モジュールに提供する
-import Control.Monad.Instances
-
 ---------------------------------------------------------------------------------------------------
+-- 型宣言
+---------------------------------------------------------------------------------------------------
+
+
 -- ObjectState型クラス ゲーム内で状態として受け渡されるキャラクターや障害物のようなオブジェクト
----------------------------------------------------------------------------------------------------
-
-class (Collision o, CollisionType o) => ObjectState o where
+class ObjectState o where
   drawObject :: GameField -> o ->  IO ()
 
 ---------------------------------------------------------------------------------------------------
--- 最低限の機能を持つObjectを標準で提供
+-- TODO アクションRPG等を作る事を考えた時、当たり判定枠が一定にはならないので
+-- CollisionSetのコレクションを扱う型クラスを考える
 ---------------------------------------------------------------------------------------------------
 
-data StandardObjectState t = StandardObjectState {
-  objectShape :: Shape,
-  objectType :: t
-  } deriving (Show, Read)
 
---当たり判定関連の型クラスインスタンス
-instance Collision (StandardObjectState t) where
-  l `collision` r = objectShape l `collision` objectShape r
-instance CollisionType t => CollisionType (StandardObjectState t) where
-  l `canOverlap` r = objectType l `canOverlap` objectType r
-
---オブジェクト型クラスのインスタンス
-instance (Show t, CollisionType t) => ObjectState (StandardObjectState t) where
-  drawObject _ o = putStrLn $ "Draw Object : " ++ show o
+-- *******************************************************
+-- *** オブジェクトそのものに当たり判定は無い　***
+-- *******************************************************
+-----------------------------------------------------------------------------------------------------
+---- 最低限の機能を持つObjectを標準で提供
+-----------------------------------------------------------------------------------------------------
+--
+--data StandardObjectState t = StandardObjectState {
+--  objectShape :: Shape,
+--  objectType :: t
+--  } deriving (Show, Read)
+--
+----当たり判定関連の型クラスインスタンス
+--instance Collision (StandardObjectState t) where
+--  l `collision` r = objectShape l `collision` objectShape r
+--instance CollisionType t => CollisionType (StandardObjectState t) where
+--  l `canOverlap` r = objectType l `canOverlap` objectType r
+--
+----オブジェクト型クラスのインスタンス
+--instance (Show t, CollisionType t) => ObjectState (StandardObjectState t) where
+--  drawObject _ o = putStrLn $ "Draw Object : " ++ show o
--- Graphics/UI/Bucephalus/Type/Collision.hs
+++ Graphics/UI/Bucephalus/Type/Collision.hs
@@ -2,16 +2,22 @@ module Graphics.UI.Bucephalus.Type.Collision(
   --型クラス
   Collision(..),
   CollisionType(..),
+  CollisionSet(..),
   --当たり判定枠提供
   Point(..),
   Rectangle(..),
   Shape(..),
   --当たり判定タイプ
-  StandardCollisionType(..)
+  StandardCollisionType(..),
+  --当たり判定コレクション
+  StandardCollisionSet(..)
   ) where
 
+--(,) a をFunctorとして利用する
+import Control.Monad.Instances
+
 ---------------------------------------------------------------------------------------------------
--- 当たり判定型クラス、Bucephalus向けに当たり判定処理を提供する	
+-- 型定義
 ---------------------------------------------------------------------------------------------------
 
 --当たり判定枠
@@ -20,7 +26,14 @@ class Collision c where
 
 --当たり判定タイプ
 class CollisionType t where
-  canOverlap :: t -> t -> Bool
+  canOverlap :: t -> t -> Bool 
+
+--当たり判定コレクション、複数の(Collision, CollisionType)のペアを一括で扱う
+class CollisionSet c where
+  --CollisionType別全ての衝突判定
+  collisionA :: CollisionType t => c -> c -> [(t, t)] 
+  --canOverlap = Falseの組み合わせのみ判定
+  collisionC :: CollisionType t => c -> c -> [(t, t)]
 
 ---------------------------------------------------------------------------------------------------
 -- 個々の当たり判定インスタンスの作成
@@ -29,7 +42,8 @@ class CollisionType t where
 
 --型定義
 data Point = Point (Int, Int) deriving (Show, Read, Eq)
- 
+
+--インスタンス作成 
 instance Collision Point where
   collision l r = l == r 
  
@@ -39,6 +53,7 @@ instance Collision Point where
 --型定義
 data Rectangle = Rectangle (Int, Int) (Int, Int) deriving (Show, Read, Eq)
 
+--インスタンス作成 
 instance Collision Rectangle where
   collision rect1 rect2 = let
     (Rectangle (x0, y0) (x1, y1)) = rect1
@@ -56,14 +71,13 @@ instance Collision Shape where
   (ShapeRectangle l) `collision` (ShapeRectangle r) = l `collision` r
 
 ---------------------------------------------------------------------------------------------------
-
----------------------------------------------------------------------------------------------------
 -- 基本的な当たり判定タイプを標準で提供
 ---------------------------------------------------------------------------------------------------
 --通常のマップ用当たり判定枠
 
 --壁、地面、キャラクター、カーソルの三種類を提供
-data StandardCollisionType = TypeWall | TypeGround | TypeChar | TypeCursor
+data StandardCollisionType = 
+  TypeWall | TypeGround | TypeChar | TypeCursor deriving (Show, Read, Eq)
 
 --キャラクターと壁／キャラクターとキャラクターは重なれない
 instance CollisionType StandardCollisionType where
@@ -74,3 +88,16 @@ instance CollisionType StandardCollisionType where
 --unitをインスタンスにする事で、メニュー等の当たり判定タイプが不要な場合に対応
 instance CollisionType () where
   _ `canOverlap` _ = True
+
+---------------------------------------------------------------------------------------------------
+-- 基本的なCollisionCollectionの型を標準で提供
+---------------------------------------------------------------------------------------------------
+
+--コレクションをリストで扱う
+data StandardCollisionSet t c = StandardCollisionSet [(t, c)]
+
+instance Functor (StandardCollisionSet t) where
+  fmap f (StandardCollisionSet x) = StandardCollisionSet $ fmap (fmap f) x
+instance CollisionSet (StandardCollisionSet t c) where
+  collisionC = undefined
+  collisionA = undefined
