c4fbbda093eb87c3871920132f00845d9a5d29bc
Tokiwo-ousaka <its.out.of.tune.this.my.music@gmail.com>
2013-02-10 23:55:32 +0900

    四角形の当たり判定の単体テストコード、HUnitを用いた形に全て書きなおし

--- Test/Type/Collision.hs
+++ Test/Type/Collision.hs
@@ -9,11 +9,41 @@ import Graphics.UI.Bucephalus.Type.Collision
 rectCollisionTests :: Test
 rectCollisionTests = "Rectangle collision tests" ~: test [
   --テストケース１ ■leftの右下の点が■rightに入りこんでいる場合のテスト
-  "Case01" ~: (Rectangle (10,10) (70,70)) `collision` (Rectangle (25,25) (100,100)) ~=? True,
+  "Case01" ~: 
+  (Rectangle (Point (10,10)) (Point (70,70))) `collision` 
+  (Rectangle (Point (25,25)) (Point (100,100))) ~=? True,
   --テストケース２　■leftに■rightが内包されている場合のテスト
-  "Case02" ~: (Rectangle (-100,-80) (70,10)) `collision` (Rectangle (0,0) (50,40)) ~=? True ,
+  "Case02" ~: 
+  (Rectangle (Point (-100,-80)) (Point (70,10))) `collision` 
+  (Rectangle (Point (0,0)) (Point (50,40))) ~=? True ,
   --テストケース３　■leftと■rightが横方向に隣り合いぶつかっていない場合のテスト
-  "Case03" ~: (Rectangle (-110,0) (-60,50)) `collision` (Rectangle (-50,0) (0,50)) ~=? False ]
+  "Case03" ~: 
+  (Rectangle (Point (-110,0)) (Point (-60,50))) `collision` 
+  (Rectangle (Point (-50,0)) (Point (0,50))) ~=? False ,
+  --テストケース４ ■leftと■rightが縦方向に隣り合いぶつかっていない場合のテスト
+  "Case04" ~: 
+  (Rectangle (Point (100,-110)) (Point (200,-60))) `collision` 
+  (Rectangle (Point (100,-50)) (Point (200,0))) ~=? False ,
+  --テストケース５ ■leftに■rightが内包されている場合のテスト
+  "Case05" ~: 
+  (Rectangle (Point (0,0)) (Point (100,100))) `collision` 
+  (Rectangle (Point (10,20)) (Point (80,90))) ~=? True ,
+  ----テストケース６ ■leftと■rightが四隅の点すべてにおいて一致(∴完全重複)している場合のテスト
+  "Case06" ~: 
+  (Rectangle (Point (0,0)) (Point (65535,65535))) `collision` 
+  (Rectangle (Point (0,0)) (Point (65535,65535))) ~=? True ,
+  --テストケース７ ■leftと■rightが■leftの右下の点■rightの左上の点で接している場合のテスト(★ぶつかってない扱い)
+  "Case07" ~: 
+  (Rectangle (Point (0,0)) (Point (255,255))) `collision` 
+  (Rectangle (Point (255,255)) (Point (65535,65535))) ~=? False ,
+  --テストケース8 ■leftと■rightが■leftの右辺■rightの左辺で接している場合のテスト(★ぶつかってない扱い)
+  "Case08" ~: 
+  (Rectangle (Point (0,0)) (Point (255,255))) `collision` 
+  (Rectangle (Point (255,0)) (Point (255,512))) ~=? False ,
+  ----テストケース9 ■leftと■rightが■leftの右辺■rightの左辺で接している場合のテスト(★ぶつかってる扱い)
+  "Case09" ~: 
+  (Rectangle (Point (0,0)) (Point (255,255))) `collision` 
+  (Rectangle (Point (254,0)) (Point (255,512))) ~=? True ]
 
 ---------------------------------------------------------------------------------------------------
 
@@ -24,73 +54,3 @@ allCollisionTests = "Collision tests" ~: test [rectCollisionTests]
 --main関数・・・テスト実行
 main :: IO ()
 main = runTestTT allCollisionTests >> return ()
-
----------------------------------------------------------------------------------------------------
--- 旧テスト要削除
----------------------------------------------------------------------------------------------------
-
- --テストケース１ ■leftの右下の点が■rightに入りこんでいる場合のテスト
-test01 :: Bool
-test01 = let
-  left = Rectangle (10,10) (70,70)
-  right = Rectangle (25,25) (100,100)
-  in (left `collision` right) == True
-  
- --テストケース２　■leftに■rightが内包されている場合のテスト
-test02 :: Bool
-test02 = let
-  left = Rectangle (-100,-80) (70,10)
-  right = Rectangle (0,0) (50,40)
-  in (left `collision` right) == True
-  
- --テストケース３　■leftと■rightが横方向に隣り合いぶつかっていない場合のテスト
-test03 :: Bool
-test03 = let
-  left = Rectangle (-110,0) (-60,50)
-  right = Rectangle (-50,0) (0,50)
-  in (left `collision` right) == False
-
- --テストケース４ ■leftと■rightが縦方向に隣り合いぶつかっていない場合のテスト
-test04 :: Bool
-test04 = let
-  left = Rectangle (100,-110) (200,-60)
-  right = Rectangle (100,-50) (200,0)
-  in (left `collision` right) == False
-  
- --テストケース５ ■leftに■rightが内包されている場合のテスト
-test05 :: Bool
-test05 = let
-  left = Rectangle (0,0) (100,100)
-  right = Rectangle (10,20) (80,90)
-  in (left `collision` right) == True
-  
- --テストケース６ ■leftと■rightが四隅の点すべてにおいて一致(∴完全重複)している場合のテスト
-test06 :: Bool
-test06 = let
-  left = Rectangle (0,0) (65535,65535)
-  right = Rectangle (0,0) (65535,65535)
-  in (left `collision` right) == True
-
- --テストケース７ ■leftと■rightが■leftの右下の点■rightの左上の点で接している場合のテスト(★ぶつかってない扱い)
-test07 :: Bool
-test07 = let
-  left = Rectangle (0,0) (255,255)
-  right = Rectangle (255,255) (65535,65535)
-  in (left `collision` right) == False
-
- --テストケース8 ■leftと■rightが■leftの右辺■rightの左辺で接している場合のテスト(★ぶつかってない扱い)
-test08 :: Bool
-test08 = let
-  left = Rectangle (0,0) (255,255)
-  right = Rectangle (255,0) (255,512)
-  in (left `collision` right) == False
-
- --テストケース9 ■leftと■rightが■leftの右辺■rightの左辺で接している場合のテスト(★ぶつかってる扱い)
-test09 :: Bool
-test09 = let
-  left = Rectangle (0,0) (255,255)
-  right = Rectangle (254,0) (255,512)
-  in (left `collision` right) == True
-
-rectTest :: (Bool, [(Int, Bool)])
-rectTest = (\l -> (all id l ,zip [1..] l)) [test01, test02, test03, test04, test05, test06, test07, test08, test09]
