7b6f5405f51451dd874165fa8c94a3c1bc6efe11
Tokiwo-ousaka <its.out.of.tune.this.my.music@gmail.com>
2013-03-17 21:23:31 +0900

    ゲームオブジェクト周りの名前の変更、機能強化

--- Graphics/UI/Bucephalus/Base/Object.hs
+++ Graphics/UI/Bucephalus/Base/Object.hs
@@ -1,27 +1,43 @@
 
-module Graphics.UI.Bucephalus.Base.Object(
-  ) where
+module Graphics.UI.Bucephalus.Base.Object where
 
 --Bucephalusモジュール
 import Graphics.UI.Bucephalus.Type.Shape
-import Graphics.UI.Bucephalus.Type.Surface
+import qualified Graphics.UI.Bucephalus.Type.InfList as INF
 
 ---------------------------------------------------------------------------------------------------
 -- 型定義
 ---------------------------------------------------------------------------------------------------
+--描画情報型クラス ...Typeモジュールに移動したほうが良いかも
 
-data Object c = Object {
-  objCollision :: c,
-  position :: (Int, Int)
+class DrawInfomation d where
+  --drawInfo :: TODO 考え中
+
+---------------------------------------------------------------------------------------------------
+-- キャラクターや障害物、画面内のイメージ画像等を保持するオブジェクト
+
+data GameObject d c = GameObject {
+  position :: (Int, Int),
+  objStatus :: ObjectStatus d c,
+  objFreames :: INF.List (ObjectStatus d c)
   }
 
 --衝突判定
-instance (Collision c, Movement c) => Collision (Object c) where
+instance (Collision c, Movement c) => Collision (GameObject d c) where
   l `collision` r = moveCollision l `collision` moveCollision r
-    where moveCollision o = move (position o) $ objCollision o
+    where moveCollision o = move (position o) $ (objCollision . objStatus) o
 
 --オブジェクト移動
-instance Movement (Object c) where
+instance Movement (GameObject d c) where
   move (x, y) o = let
     (ox, oy) = position o
     in o { position = (x + ox, y + oy) }
+
+---------------------------------------------------------------------------------------------------
+--オブジェクトの瞬間的な状態
+
+data ObjectStatus d c = ObjectStatus {
+  objCollision :: c,
+  objDrawInfo :: d
+  }
+
